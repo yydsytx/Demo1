b1f59b94223a2385d5a036659e8f687c6a589be3
ytx0904 <ytx0904@ytx.local>
2019-05-28 16:58:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ytx0904/demo1/MathUtils.java
@@ -0,0 +1,11 @@
+package com.example.ytx0904.demo1;
+
+/**
+ * Created by ytx0904 on 2019/5/28.
+ */
+
+public class MathUtils {
+    public static int add(int a,int b){
+        return  a+b;
+    }
+}
